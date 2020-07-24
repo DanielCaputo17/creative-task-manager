073187c7b8da4ed3409eb5c8a5d58a86fc1bcc18
DanielCaputo17 <danielcaputo17@gmail.com>
2020-07-24 15:51:31 -0400

    Updates

---
+++ precache-manifest.4ccfde8984ca80c8bbd7da03c9f5fac8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "063109610f45f91e66f9e81d620975bf",
+    "url": "/creative-task-manager/index.html"
+  },
+  {
+    "revision": "229b348af4dada17ef6c",
+    "url": "/creative-task-manager/static/css/main.1cf384d2.chunk.css"
+  },
+  {
+    "revision": "a0011a0cf9b7e040d636",
+    "url": "/creative-task-manager/static/js/2.9fb9651a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/creative-task-manager/static/js/2.9fb9651a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "229b348af4dada17ef6c",
+    "url": "/creative-task-manager/static/js/main.2e6d1d5f.chunk.js"
+  },
+  {
+    "revision": "7d717c329725659f2ed9",
+    "url": "/creative-task-manager/static/js/runtime-main.d03ce020.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/creative-task-manager/static/media/logo.5d5d9eef.svg"
+  }
+]);
